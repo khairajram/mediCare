ba56cdc91ac838c52f219122cc1990ea655bc6af
khaiRaj <khairajramtard@gmail.com>
2025-08-11 18:17:08 +0530

    --

--- app/api/(auth)/pet/[id]/route.ts
+++ app/api/(auth)/pet/[id]/route.ts
@@ -3,44 +3,44 @@ import { z } from "zod"
 
 const prisma = new PrismaClient();
 
-export async function GET(req : Request , { params } : { params : { id : string }}){
-  try{
+// export async function GET(req : Request , { params } : { params : { id : string }}){
+//   try{
 
-    const id = params.id;
+//     const id = params.id;
 
-    const pet = await prisma.pet.findUnique({
-      where : { id }
-    });
+//     const pet = await prisma.pet.findUnique({
+//       where : { id }
+//     });
 
-    if (!pet) {
-      return new Response(JSON.stringify({ message: "Pet not found" }), {
-        status: 404,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-
-
-    return new Response(JSON.stringify({
-      message: "pet given",
-      pet
-    }), {
-      status: 201,
-      headers: { "Content-Type": "application/json" },
-    });
+//     if (!pet) {
+//       return new Response(JSON.stringify({ message: "Pet not found" }), {
+//         status: 404,
+//         headers: { "Content-Type": "application/json" },
+//       });
+//     }
 
 
-  }catch(err : any){
-    console.error("GET pets [id] error:", err);
+//     return new Response(JSON.stringify({
+//       message: "pet given",
+//       pet
+//     }), {
+//       status: 201,
+//       headers: { "Content-Type": "application/json" },
+//     });
 
-    return new Response(JSON.stringify({
-      message: "Internal Server Error",
-      error: err.message,
-    }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-}
+
+//   }catch(err : any){
+//     console.error("GET pets [id] error:", err);
+
+//     return new Response(JSON.stringify({
+//       message: "Internal Server Error",
+//       error: err.message,
+//     }), {
+//       status: 500,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
 
 export async function PUT(req : Request , { params } : { params : { id : string }}){
   try{
