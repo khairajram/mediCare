9a28f30d30ecf8abf8f83e3f1e3b0560134a4cb7
khaiRaj <khairajramtard@gmail.com>
2025-08-11 19:29:18 +0530

    --

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { getServerSession } from "next-auth";
-import { ThemeToggle } from "./theme-toggle";
-
 
 export default async function Home() {
-  const session = await getServerSession();
   
   return (
     <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black text-black dark:text-white transition-all duration-300">
